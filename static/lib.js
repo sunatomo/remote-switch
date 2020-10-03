4ac9b57281ffe78aa2975f905ac2328edc1d7ae7
tomok <tomokisunaga@live.com>
2020-10-03 11:13:55 +0900

    キーボード操作対応

---
+++ static/lib.js
@@ -0,0 +1,31 @@
+buttons = [
+    'B',
+    'A',
+    'Y',
+    'X',
+    'L',
+    'R',
+    'ZL',
+    'ZR',
+    '-',
+    '+',
+    'L-Stick',
+    'R-Stick',
+    'Up',
+    'Down',
+    'Left',
+    'Right',
+    'Home',
+    'Capture'
+];
+
+axes = [
+    'L-Horizon',
+    'L-Vertical',
+    'R-Horizon',
+    'R-Vertical'
+];
+
+function L(ja, en) {
+    return window.navigator.language == 'ja'? ja: en;
+}
